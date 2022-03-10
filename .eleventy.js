809c51378e990b2594358bcf95e94c40b2e997bc
Robert Zhu <robzhu@gmail.com>
2022-03-10 15:22:07 -0800

    Moves images next to content

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,14 @@
 module.exports = config => {
   config.addPassthroughCopy("static");
-  config.addPassthroughCopy("img");
   config.addPassthroughCopy("favicon.ico");
-  config.addPassthroughCopy({"images": "img"});
 
-  config.addPassthroughCopy("**/*.jpg");
+  config.addPassthroughCopy("readme/**/*.jpg");
+  config.addPassthroughCopy("readme/**/*.jpeg");
+  config.addPassthroughCopy("readme/**/*.png");
+  config.addPassthroughCopy("readme/**/*.webp");
+
+  config.addPassthroughCopy("content/**/*.jpg");
+  config.addPassthroughCopy("content/**/*.jpeg");
+  config.addPassthroughCopy("content/**/*.png");
+  config.addPassthroughCopy("content/**/*.webp");
 };
